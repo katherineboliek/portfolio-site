604f12b9884c32dcb744f54b42d3b9aa0a700845
katherineboliek <katherineboliek@gmail.com>
2016-03-01 18:42:48 -0500

    Added dropdown menu & styling.

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,24 @@ $(function(){
 	$('#port').mixItUp();
 
 });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropdown() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.trigger')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
